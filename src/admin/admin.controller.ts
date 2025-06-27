318b3d10e36e76fce3dc41dc4d3a5e7d397aab21
yujeen02 <yujin5479@gmail.com>
2025-06-27 10:53:17 +0900

    feat. 관리자의 유저 권한 승인

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseIntPipe,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { AdminGuard } from 'src/common/auth/guard/admin.guard';
+import { AdminService } from './admin.service';
+import { UserAccept } from 'src/common/users/users.entity';
 
 @Controller('admin')
-export class AdminController {}
+@UseGuards(AdminGuard)
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  // 유저 권한 승인
+  @Patch('users/:id/accept-status')
+  async updateUserAcceptStatus(
+    @Param('id', ParseIntPipe) userId: number,
+    @Body('status') status: string,
+  ) {
+    if (status !== UserAccept.ACCEPT && status !== UserAccept.REJECT) {
+      throw new Error(
+        '유효하지 않은 상태 값입니다. accept 또는 reject만 가능합니다.',
+      );
+    }
+
+    const updatedUser = await this.adminService.updateUserAcceptStatus(
+      userId,
+      status as UserAccept.ACCEPT | UserAccept.REJECT,
+    );
+
+    return {
+      message: `유저 acceptStatus가 ${status}로 변경되었습니다.`,
+      user: updatedUser,
+    };
+  }
+}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User, UserAccept } from 'src/common/users/users.entity';
+import { UsersService } from 'src/common/users/users.service';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  // 유저 승인
+  async updateUserAcceptStatus(
+    userId: number,
+    status: UserAccept.ACCEPT | UserAccept.REJECT,
+  ): Promise<User> {
+    const user = await this.userRepo.findOne({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundException('유저를 찾을 수 없습니다.');
+    }
+
+    if (user.acceptStatus !== UserAccept.PENDING) {
+      throw new BadRequestException(
+        `현재 상태가 PENDING이 아니므로 변경할 수 없습니다. 현재 상태: ${user.acceptStatus}`,
+      );
+    }
+
+    user.acceptStatus = status;
+
+    return await this.userRepo.save(user);
+  }
+}
